668e11d11e201f5075a556a10aee0f718d3e1992
Janblack07 <jandryjacob20@gmail.com>
2026-02-28 17:56:14 -0500

    fix: se movio la imagen

--- resources/views/components/application-logo.blade.php
+++ resources/views/components/application-logo.blade.php
@@ -1,5 +1,5 @@
 <img
-  src="{{ asset('build/assets/LogoDistribuidora.png') }}"
+  src="{{ asset('LogoDistribuidora.png') }}"
   alt="{{ config('app.name', 'OxygenDispatch') }}"
   {{ $attributes->merge(['class' => 'h-10 w-auto object-contain']) }}
 />
