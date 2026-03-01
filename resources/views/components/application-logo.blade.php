8806f3203a0150232b0dc5a4f483773d6224834a
Janblack07 <jandryjacob20@gmail.com>
2026-03-01 17:28:39 -0500

    Fix: imagen

--- resources/views/components/application-logo.blade.php
+++ resources/views/components/application-logo.blade.php
@@ -1,5 +1,5 @@
 <img
-  src="{{ asset('/public/build/assets/LogoDistribuidora.png') }}"
+  src="{{ asset('https://res.cloudinary.com/dv2gulc60/image/upload/v1772404076/OxigenDispatch/Logo_Distribuidora_tmn7yp.png') }}"
   alt="{{ config('app.name',  'OxygenDispatch') }}"
   {{ $attributes->merge(['class' => 'h-10 w-auto object-contain']) }}
 />
