71c14df3a0a589b23fc90c61918d7dfa28e625a1
Janblack07 <jandryjacob20@gmail.com>
2026-03-23 20:10:00 -0500

    Fix: Se cambio el color del boton de filtro

--- resources/views/dispatches/create.blade.php
+++ resources/views/dispatches/create.blade.php
@@ -235,7 +235,7 @@
                                 <button
                                     type="button"
                                     id="apply-tank-filters"
-                                    class="inline-flex items-center px-3 py-2 bg-slate-800 text-white rounded-md font-semibold text-xs uppercase tracking-widest hover:bg-slate-700 focus:outline-none focus:ring-2 focus:ring-slate-500 focus:ring-offset-2"
+                                    class="inline-flex w-full justify-center items-center px-3 py-2 bg-gray-900 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-gray-800"
                                 >
                                     Filtrar tanques
                                 </button>
